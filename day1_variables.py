b203c8a5e7d49c458d21867e8223f788d298ebab
Roshan Rajgarhia <rrajgarhia1008@gmail.com>
2026-01-10 00:27:14 +0530

    Day 1: Variables and data types practice

---
+++ day1_variables.py
@@ -0,0 +1,13 @@
+name= "Roshan"
+age= 24
+height= 5.5
+is_student= False
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+age= input ("Enter your age: ")
+print ("After 10 years your age will be ", int(age) + 10)
+
