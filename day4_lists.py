8d965b9c37d5529d9020ed20e778a09fefa966e8
Roshan Rajgarhia <rrajgarhia1008@gmail.com>
2026-01-11 21:44:17 +0530

    Day 4: lists and data processing logic

--- day4_lists.py
+++ day4_lists.py
@@ -1,4 +1,5 @@
-age =[18,12,45,23,76,45,87,9,25]
+age =[18,24,46,12,45,23,76,45,87,9,25]
+'''
 minor= 0
 adult=0
 senior=0
@@ -13,6 +14,19 @@ for x in age:
         print("Senior")
         senior += 1
 
+
 print("Minors: ", minor)
 print("Adult: ", adult)
 print("Senior: ", senior)
+'''
+a=0
+b=0
+for x in age:
+    if x%2==0:
+        a=a+1
+        
+    else:
+        b=b+1
+        
+print("even ages: ",a)
+print("odd ages: ",b)
